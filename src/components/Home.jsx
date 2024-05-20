116178b26e6fec11dd806849cc9d7b02ed7bb511
let-sampaio <leticia.sampaio.souza08@aluno.ifce.edu.br>
2024-05-20 19:04:55 -0300

    Newsletter✨

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+    
+    <div className="container">
+        <div className="image"><img src="src\img\illustration-sign-up-desktop.svg" alt="foto-capa" /></div>
+        <h1 className="titulo">Stay updated!</h1>
+        <p className="subtitulo">Join 60,000+ product managers
+        receiving monthly updates on:</p>
+        <ul className="lista">
+            <img src="src\img\icon-list.svg" alt="icon-list" />
+            <li>Product discovery and building what matters</li>
+            <img src="src\img\icon-list.svg" alt="icon-list" />
+            <li>Measuring to ensure updates are a sucess</li>
+            <img src="src\img\icon-list.svg" alt="icon-list" />
+            <li>And much more!</li>
+        </ul>
+        <p className="email-label">Email address</p>
+        <input type="text" className="email-box" placeholder='email@company.com' />
+        <button className='btn-sub'>Subscribe to monthly newsletter</button>
+
+    </div>
+    </>
+    
+  )
+}
+
+export default Home
