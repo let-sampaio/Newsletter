5c2d240880f04ece46a5b9a9d5d9bee70a8681d6
let-sampaio <leticia.sampaio.souza08@aluno.ifce.edu.br>
2024-05-28 14:25:30 -0300

    Newsletter✨

--- src/components/Submitte.jsx
+++ src/components/Submitte.jsx
@@ -6,9 +6,10 @@ import { Link } from 'react-router-dom';
 function Submitte() {
   return (
     <div className="caixa">
+        <img src="src\img\icon-success.svg" alt="check-image" className="check-image" />
         <h1 className="txt">Thanks for subscribing!</h1>
-        <p className="txt-menor">A confirmation email has been sent to 'email colocado'
-        .Please open it and click the button inside to confirm your subscription.</p>
+        <p className="txt-menor">A confirmation email has been sent to <b>leticia.souzza14@gmail.com</b>
+        . Please open it and click the button inside to confirm your subscription.</p>
         <Link to="/" className='btn-dismiss'>Dismiss message</Link>
     </div>
   )
